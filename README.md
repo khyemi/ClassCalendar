b7be708ea6214d646e2920d41ee1ed7c23d33331
khyemi <khyemi0801@gmail.com>
2017-06-22 17:19:35 -0400

    unwanted files

--- README.md
+++
@@ -1,3 +0,0 @@
-# ClassCalendar
-Shuai He, Hye Mi Kim  
-Simple calendar view of classes built in JavaScript utilizing Google Map API
--- UMNClassCalendar/admin.php
+++
@@ -1,5 +0,0 @@
- <?php
-  include '../views/login view.php';
-  include '../views/users_view.php';
-  header("LOCATION:./views/users_view.php");
- ?>
